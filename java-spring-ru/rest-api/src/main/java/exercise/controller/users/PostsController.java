2f8c012e67977c9ed9122efca2eb9595519a8775
@hexlet/cli <support@hexlet.io>
2024-07-04 23:48:10 +0300

    submit java-spring-ru/rest-api

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,48 +1,33 @@
 package exercise.controller.users;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import exercise.model.Post;
 import exercise.Data;
 
 // BEGIN
 @RestController
+@RequestMapping("/api")
 public class PostsController {
 
-
-    @GetMapping("/api/users/{id}/posts")
-    public List<Post> show(@PathVariable String id) {
-        List<Post> newListPost = Data.getPosts();
-        List<Post> posts = new ArrayList<>();
-        for (Post post : newListPost) {
-            if (post.getUserId() == Integer.parseInt(id)) {
-                posts.add(post);
-            }
-        }
-        return posts;
+    @GetMapping("/users/{id}/posts")
+    public List<Post> show(@PathVariable("id") int id) {
+        return Data.getPosts().stream()
+                .filter(s->s.getUserId()==id)
+                .collect(Collectors.toList());
     }
 
-    @PostMapping("/api/users/{id}/posts")
-    public ResponseEntity<Post> create(@RequestBody Post post, @PathVariable String id) {
-        Post post1 = post;
-        post1.setUserId(Integer.parseInt(id));
-        post1.setSlug(post.getSlug());
-        post1.setTitle(post.getTitle());
-        post1.setBody(post.getBody());
-        return ResponseEntity.status(HttpStatus.CREATED).body(post1);
+    @PostMapping("/users/{id}/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@PathVariable("id") int id,
+                       @RequestBody Post post) {
+        post.setUserId(id);
     }
 }
 
